c12013fe8a17f9654660fc2986c40abd4d1cef6e
Rhys Hawkins <Rhys.Hawkins@anu.edu.au>
2018-04-09 15:23:07 +1000

    License
    O#	modified:   wavetree/wavetree_prior.h

--- wavetree/wavetree2d_sub.h
+++ wavetree/wavetree2d_sub.h
@@ -1,3 +1,28 @@
+//
+//    Wavetree Library : A library for performed trans-dimensional tree inversion,
+//    See
+//
+//      R Hawkins and M Sambridge, "Geophysical imaging using trans-dimensional trees",
+//      Geophysical Journal International, 2015, 203:2, 972 - 1000,
+//      https://doi.org/10.1093/gji/ggv326
+//    
+//    Copyright (C) 2014 - 2018 Rhys Hawkins
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
 #ifndef wavetree2d_sub_h
 #define wavetree2d_sub_h
 
